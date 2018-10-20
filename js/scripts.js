39579bc5a47f7eda9d255a51e254141a3fb8c2bf
Edin Filipovic <edin.filipovic89@gmail.com>
2018-10-19 23:30:03 -0700

    updated js file, but not able to get response on show. trying to troubleshoot...

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,60 @@
 $(document).ready (function() {
-  $("#formOne").submit (function(event) {
+  $("#needs-validation").submit (function(event) {
+    var forms = document.getElementsByClassName('needs-validation');
+    console.log(forms);  
     var firstNameInput = $("input#firstName").val();
     var lastNameInput = $("input#lastName").val();
-    var verbInput = $("input#verb").val();
-    var clothingInput = $("input#clothing").val();
-    var nounInput = $("input#noun").val();
-    var pluralnoun1Input = $("input#pluralnoun1").val();
-    var bodyInput = $("input#body").val();
-    var pluralnoun2Input = $("input#pluralnoun2").val();
-    var bodypartInput = $("input#bodypart").val();
-    var noun2Input = $("input#noun2").val();
-    var noun3Input = $("input#noun3").val();
-    var actionInput = $("input#action").val();
-    var adjective2Input = $("input#adjective2").val();
-    var adjective3Input = $("input#adjective3").val();
+    var q1answer1Input = $("input#q1answer1").val();
+    var q1answer2Input = $("input#q1answer2").val();
+    var q1answer3Input = $("input#q1answer3").val();
+    var q1answer4Input = $("input#q1answer4").val();
+    var q2answer1Input = $("input#q2answer1").val();
+    var q2answer2Input = $("input#q2answer2").val();
+    var q2answer3Input = $("input#q2answer3").val();
+    var q2answer4Input = $("input#q2answer4").val();
+    var q3answer1Input = $("input#q3answer1").val();
+    var q3answer2Input = $("input#q3answer2").val();
+    var q3answer3Input = $("input#q3answer3").val();
+    var q4answer1Input = $("input#q4answer1").val();
+    var q4answer2Input = $("input#q4answer2").val();
+    var q4answer3Input = $("input#q4answer3").val();
+    var q5answer1Input = $("input#q5answer1").val();
+    var q5answer2Input = $("input#q5answer2").val();
+    var q5answer3Input = $("input#q5answer3").val();
+    var q5answer4Input = $("input#q5answer4").val();
+    var q5answer5Input = $("input#q5answer5").val();
+    var q5answer6Input = $("input#q5answer6").val();
 
-    $(".pluralnoun").text(pluralnounInput);
-    $(".adverb").text(adverbInput);
-    $(".verb").text(verbInput);
-    $(".clothing").text(clothingInput);
-    $(".noun").text(nounInput);
-    $(".pluralnoun1").text(pluralnoun1Input);
-    $(".body").text(bodyInput);
-    $(".pluralnoun2").text(pluralnoun2Input);
-    $(".bodypart").text(bodypartInput);
-    $(".noun2").text(noun2Input);
-    $(".noun3").text(noun3Input);
-    $(".action").text(actionInput);
-    $(".adjective2").text(adjective2Input);
-    $(".adjective3").text(adjective3Input);
 
-    $("#submit").click(function(){
-        alert($('input[name=codeFun]').val());
+
+    $(".lastName").text(firstNameInput);
+    $(".lastName").text(lastNameInput);
+    $(".q1answer1").text(q1answer1Input);
+    $(".q1answer2").text(q1answer2Input);
+    $(".q1answer3").text(q1answer3Input);
+    $(".q1answer4").text(q1answer4Input);
+    $(".q2answer1").text(q2answer1Input);
+    $(".q2answer2").text(q2answer2Input);
+    $(".q2answer3").text(q2answer3Input);
+    $(".q2answer4").text(q2answer4Input);
+    $(".q3answer1").text(q3answer1Input);
+    $(".q3answer2").text(q3answer2Input);
+    $(".q3answer3").text(q3answer3Input);
+    $(".q4answer1").text(q4answer1Input);
+    $(".q4answer2").text(q4answer2Input);
+    $(".q4answer3").text(q4answer3Input);
+    $(".q5answer1").text(q5answer1Input);
+    $(".q5answer2").text(q5answer2Input);
+    $(".q5answer3").text(q5answer3Input);
+    $(".q5answer4").text(q5answer4Input);
+    $(".q5answer5").text(q5answer5Input);
+    $(".q5answer6").text(q5answer6Input);
+
+    $("#questionnaire").show();
+
+    event.preventDefault();
     });
  });
 
-    event.preventDefault();
-
-  });
-});
+ /*$("#submit").click(function(){
+ text($('input[name=codeFun]').val());*/
