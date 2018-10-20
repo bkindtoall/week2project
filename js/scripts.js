20f24fe6025721b3a3a9942d42676e694ec92cfb
Edin Filipovic <edin.filipovic89@gmail.com>
2018-10-19 20:34:15 -0700

    fixed radios so that each question would have one answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,41 @@
-// $(document).ready(function() {
-  (function() {
-  'use strict';
-  window.addEventListener('load', function() {
+$(document).ready (function() {
+  $("#formOne").submit (function(event) {
+    var firstNameInput = $("input#firstName").val();
+    var lastNameInput = $("input#lastName").val();
+    var verbInput = $("input#verb").val();
+    var clothingInput = $("input#clothing").val();
+    var nounInput = $("input#noun").val();
+    var pluralnoun1Input = $("input#pluralnoun1").val();
+    var bodyInput = $("input#body").val();
+    var pluralnoun2Input = $("input#pluralnoun2").val();
+    var bodypartInput = $("input#bodypart").val();
+    var noun2Input = $("input#noun2").val();
+    var noun3Input = $("input#noun3").val();
+    var actionInput = $("input#action").val();
+    var adjective2Input = $("input#adjective2").val();
+    var adjective3Input = $("input#adjective3").val();
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    console.log(forms);
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      console.log(form);
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
+    $(".pluralnoun").text(pluralnounInput);
+    $(".adverb").text(adverbInput);
+    $(".verb").text(verbInput);
+    $(".clothing").text(clothingInput);
+    $(".noun").text(nounInput);
+    $(".pluralnoun1").text(pluralnoun1Input);
+    $(".body").text(bodyInput);
+    $(".pluralnoun2").text(pluralnoun2Input);
+    $(".bodypart").text(bodypartInput);
+    $(".noun2").text(noun2Input);
+    $(".noun3").text(noun3Input);
+    $(".action").text(actionInput);
+    $(".adjective2").text(adjective2Input);
+    $(".adjective3").text(adjective3Input);
+
+    $("#submit").click(function(){
+        alert($('input[name=codeFun]').val());
     });
-  }, false);
-})();
+ });
 
-var flavor = $("input:radio[name=flavor]:checked").val();
+    event.preventDefault();
 
-// });
+  });
+});
