72f383cf465f5602811c68a8c76d76971d941acc
Edin Filipovic <edin.filipovic89@gmail.com>
2018-10-24 22:35:19 -0700

    enable correct response after adding variables and for loops with ifs

--- js/scripts.js
+++ js/scripts.js
@@ -3,62 +3,57 @@ $(document).ready(function() {
     event.preventDefault();
     var firstNameInput = $("input#firstName").val();
     var lastNameInput = $("input#lastName").val();
-    /*var q1answer1Input = $("input#q1answer1").val();
-    var q1answer2Input = $("input#q1answer2").val();
-    var q1answer3Input = $("input#q1answer3").val();
-    var q1answer4Input = $("input#q1answer4").val();
-    var q2answer1Input = $("input#q2answer1").val();
-    var q2answer2Input = $("input#q2answer2").val();
-    var q2answer3Input = $("input#q2answer3").val();
-    var q2answer4Input = $("input#q2answer4").val();
-    var q3answer1Input = $("input#q3answer1").val();
-    var q3answer2Input = $("input#q3answer2").val();
-    var q3answer3Input = $("input#q3answer3").val();
-    var q4answer1Input = $("input#q4answer1").val();
-    var q4answer2Input = $("input#q4answer2").val();
-    var q4answer3Input = $("input#q4answer3").val();
-    var q5answer1Input = $("input#q5answer1").val();
-    var q5answer2Input = $("input#q5answer2").val();
-    var q5answer3Input = $("input#q5answer3").val();
-    var q5answer4Input = $("input#q5answer4").val();
-    var q5answer5Input = $("input#q5answer5").val();
-    var q5answer6Input = $("input#q5answer6").val();*/
+    var q1 = document.getElementsByName('q1CodeFun');
+    var q2 = document.getElementsByName('q2CodeFun');
+    var q3 = document.getElementsByName('q3CodeFun');
+    var q4 = document.getElementsByName('q4CodeFun');
+    var q5 = document.getElementsByName('q5CodeFun');
+    var q6 = document.getElementsByName('q6CodeFun');
+
+    var didCheck1;
+    var didCheck2;
+    var didCheck3;
+    var didCheck4;
+    var didCheck5;
+
 
     $(".firstName").text(firstNameInput);
     $(".lastName").text(lastNameInput);
-    /*$(".q1answer1").text(q1answer1Input);
-    $(".q1answer2").text(q1answer2Input);
-    $(".q1answer3").text(q1answer3Input);
-    $(".q1answer4").text(q1answer4Input);
-    $(".q2answer1").text(q2answer1Input);
-    $(".q2answer2").text(q2answer2Input);
-    $(".q2answer3").text(q2answer3Input);
-    $(".q2answer4").text(q2answer4Input);
-    $(".q3answer1").text(q3answer1Input);
-    $(".q3answer2").text(q3answer2Input);
-    $(".q3answer3").text(q3answer3Input);
-    $(".q4answer1").text(q4answer1Input);
-    $(".q4answer2").text(q4answer2Input);
-    $(".q4answer3").text(q4answer3Input);
-    $(".q5answer1").text(q5answer1Input);
-    $(".q5answer2").text(q5answer2Input);
-    $(".q5answer3").text(q5answer3Input);
-    $(".q5answer4").text(q5answer4Input);
-    $(".q5answer5").text(q5answer5Input);
-    $(".q5answer6").text(q5answer6Input);*/
 
-    $("input[type='radio']").click(function(){
-      var input =
-    $("input[name='q1CodeFun']:checked").val();
-      if(radioValue){
-        alert("You chose: " + radioValue);
+    for(var i=0; i < q1.length; i++) {
+        if(q1[i].checked){
+            didCheck1 = q1[i].value;
+        }
       }
+        for(var i=0; i < q2.length; i++) {
+        if(q2[i].checked){
+            didCheck2 = q2[i].value;
+            console.log(didCheck2)
+
+        }
+      }
+    for(var i=0; i < q3.length; i++) {
+        if(q3[i].checked){
+            didCheck3 = q3[i].value;
+            console.log(didCheck3)
+        }
+      }
+    for(var i=0; i < q4.length; i++) {
+        if(q4[i].checked){
+            didCheck4 = q4[i].value;
+            console.log(didCheck4)
+        }
+      }
+    for(var i=0; i < q5.length; i++) {
+        if(q5[i].checked){
+            didCheck5 = q5[i].value;
+            console.log(didCheck5)
+        }
+      }
+
     });
 
     $("#questionnaire").show();
 
-    });
  });
 
- /*$("#submit").click(function(){
- text($('input[name=codeFun]').val());*/
---
+++ readme.md
@@ -0,0 +1,29 @@
+# fury-porfolio
+
+#### A Webpage about me and the projects I've done in week 1 of Epicodus
+
+#### By Patricia Filipovic
+
+## Description
+
+Webpage with name banner, an image of me. A list of projects I've created with active links to their GitHub pages sites or GitHub repositories. Including a brief description of the project and what languages it uses.
+An “About Me” section. My background (education, job experience, or why did you decide to take a class on programming, etc). My current interests/hobbies/skills.
+
+https://bkindtoall.gitHub.io/fury-portfolio
+
+
+## Support and contact details
+
+Contact Patricia via email: bkindtoall@gmail.com
+
+## Technologies Used
+
+CSS
+HTML
+GIT
+GitHub
+Bootstrap
+
+*This software is licensed under the MIT license.*
+
+Copyright (c) 2018 PF
